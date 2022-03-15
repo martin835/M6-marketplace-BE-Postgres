c77187f96f99d644e2e41245e1830f07fd012953
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-15 16:42:39 +0100

    CRUD for products working

--- src/services/product.js
+++ src/services/product.js
@@ -14,8 +14,6 @@ productsRouter.get("/", async (req, res, next) => {
   }
 });
 
-export default productsRouter;
-
 //2 POST a product
 productsRouter.post("/", async (req, res, next) => {
   try {
@@ -37,3 +35,50 @@ productsRouter.post("/", async (req, res, next) => {
     console.log(error);
   }
 });
+
+//3 PUT - EDIT product
+productsRouter.put("/:id", async (req, res, next) => {
+  console.log("PING - This is a request to edit a product");
+  try {
+    const data = await pool.query(
+      "UPDATE product SET product_name=$1, image=$2, description=$3, price=$4 WHERE product_id=$5 RETURNING *;",
+      [
+        req.body.product_name,
+        req.body.image,
+        req.body.description,
+        req.body.price,
+        req.params.id,
+      ]
+    );
+
+    const isUpdated = data.rowCount > 0;
+
+    if (isUpdated) {
+      res.status(200).send(data.rows[0]);
+    } else {
+      res.status(404).send({ message: "Cannot be edited - Product not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//4 DELETE a product
+
+productsRouter.delete("/:id", async (req, res, next) => {
+  console.log("PING - this is a request to delete a product");
+  try {
+    const data = await pool.query("DELETE FROM product WHERE product_id=$1;", [
+      req.params.id,
+    ]);
+
+    const isDeleted = data.rowCount > 0;
+
+    if (isDeleted) res.status(204).send({ message: "Product deleted" });
+    else res.status(404).send({ message: "Product not found" });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+export default productsRouter;
